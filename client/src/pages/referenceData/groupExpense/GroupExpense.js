97ffd1080c7674a1f6126fe73c3c0aa8ca096ae1
sershishkov <ser.shishkov@gmail.com>
2021-02-14 10:35:10 +0200

    F refData GroupExpense

--- client/src/pages/referenceData/groupExpense/GroupExpense.js
+++ client/src/pages/referenceData/groupExpense/GroupExpense.js
@@ -1,12 +1,10 @@
-import React, { useEffect, useState, useLayoutEffect } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import { setNameOfPage } from '../../../store/actions/nameOfPage';
 import {
-  add__GROUP_EXPENSE,
   getAll__GROUP_EXPENSE,
-  getOne__GROUP_EXPENSE,
-  update__GROUP_EXPENSE,
   delete__GROUP_EXPENSE,
 } from '../../../store/actions/referenceData/groupExpense';
 
@@ -21,14 +19,7 @@ import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import Tooltip from '@material-ui/core/Tooltip';
-
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -45,110 +36,23 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const GroupExpense = ({
-  add__GROUP_EXPENSE,
+  setNameOfPage,
   getAll__GROUP_EXPENSE,
-  getOne__GROUP_EXPENSE,
-  update__GROUP_EXPENSE,
   delete__GROUP_EXPENSE,
 
   state__GROUP_EXPENSE,
 }) => {
   const classes = useStyles();
-  const [openDialog, setOpenDialog] = useState(false);
-
-  const [formData, setFormData] = useState({
-    name__Group_Expense: '',
-  });
-
-  const { name__Group_Expense } = formData;
-
-  const [editId, setEditId] = useState('');
-  const [buttonText, setButtonText] = useState('');
-
-  const [name__GROUP_EXPENSE_Helper, set_name__GROUP_EXPENSE_Helper] = useState(
-    ''
-  );
-
-  const clearFormData = () => {
-    setFormData({
-      name__Group_Expense: '',
-    });
-    setEditId('');
-  };
 
   useEffect(() => {
+    setNameOfPage('Группы затрат');
     getAll__GROUP_EXPENSE();
-
-    clearFormData();
-
-    return () => {
-      clearFormData();
-    };
-  }, [getAll__GROUP_EXPENSE]);
-
-  useLayoutEffect(() => {
-    if (state__GROUP_EXPENSE.one__GROUP_EXPENSE) {
-      // console.log(state__GROUP_EXPENSE.one__GROUP_EXPENSE);
-
-      setFormData({
-        name__Group_Expense: state__GROUP_EXPENSE.one__GROUP_EXPENSE
-          .name__Group_Expense
-          ? state__GROUP_EXPENSE.one__GROUP_EXPENSE.name__Group_Expense
-          : '',
-      });
-    }
-  }, [state__GROUP_EXPENSE.one__GROUP_EXPENSE]);
+  }, [setNameOfPage, getAll__GROUP_EXPENSE]);
 
   const onDeleteItem = (id) => {
     delete__GROUP_EXPENSE(id);
-    setEditId('');
-  };
-  const onSubmit = () => {
-    if (buttonText === 'Редактировать' && editId) {
-      update__GROUP_EXPENSE(editId, name__Group_Expense);
-    } else if (buttonText === 'Добавить') {
-      add__GROUP_EXPENSE(name__Group_Expense);
-    }
-
-    setOpenDialog(false);
-    clearFormData();
-    setEditId('');
   };
 
-  const buttonAddHandler = () => {
-    setOpenDialog(true);
-    setButtonText('Добавить');
-    setEditId('');
-    clearFormData();
-  };
-
-  const buttonEditHandler = (id) => {
-    getOne__GROUP_EXPENSE(id);
-    setEditId(id);
-    setButtonText('Редактировать');
-
-    setOpenDialog(true);
-  };
-
-  const onChangeHandler = (event) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
-
-    let valid;
-    switch (event.target.id) {
-      case 'name__Group_Expense':
-        valid = event.target.value.length >= 3;
-        if (!valid) {
-          set_name__GROUP_EXPENSE_Helper('Минимальная длина 3 знака');
-        } else {
-          set_name__GROUP_EXPENSE_Helper('');
-        }
-        break;
-
-      default:
-        break;
-    }
-  };
-  // console.log(state__GROUP_EXPENSE.array__GROUP_EXPENSE);
   const rows =
     state__GROUP_EXPENSE.array__GROUP_EXPENSE &&
     state__GROUP_EXPENSE.array__GROUP_EXPENSE.length > 0
@@ -157,7 +61,10 @@ const GroupExpense = ({
             name__Group_Expense: item.name__Group_Expense,
 
             edit: (
-              <IconButton onClick={() => buttonEditHandler(item._id)}>
+              <IconButton
+                component={Link}
+                href={`/reference-data/group-expense/${item._id}`}
+              >
                 <EditIcon color='primary' />
               </IconButton>
             ),
@@ -223,7 +130,8 @@ const GroupExpense = ({
         <Fab
           color='secondary'
           aria-label='add'
-          onClick={() => buttonAddHandler()}
+          component={Link}
+          href={`/reference-data/group-expense/add`}
         >
           <AddIcon />
         </Fab>
@@ -238,76 +146,13 @@ const GroupExpense = ({
       <Grid item className={classes.item}>
         {myMaterialTable}
       </Grid>
-      <Dialog
-        open={openDialog}
-        onClose={() => {
-          setOpenDialog(false);
-          clearFormData();
-        }}
-        aria-labelledby='form-dialog-title'
-        aria-describedby='alert-dialog-description'
-        fullWidth
-      >
-        <DialogTitle id='form-dialog-title'>Группа</DialogTitle>
-        <DialogContent>
-          <DialogContentText id='alert-dialog-description'>
-            {buttonText ? buttonText : 'Поехали'}
-          </DialogContentText>
-          <Grid
-            container
-            direction='column'
-            justify='flex-start'
-            alignItems='center'
-          >
-            <Grid item className={classes.dialogItem}>
-              <TextField
-                autoFocus
-                id='name__Group_Expense'
-                name='name__Group_Expense'
-                label='Группа расходов'
-                type='text'
-                value={name__Group_Expense ? name__Group_Expense : ''}
-                error={name__GROUP_EXPENSE_Helper.length !== 0}
-                helperText={name__GROUP_EXPENSE_Helper}
-                fullWidth
-                autoComplete='text'
-                onChange={(e) => onChangeHandler(e)}
-              />
-            </Grid>
-          </Grid>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            variant='contained'
-            onClick={() => {
-              setOpenDialog(false);
-              clearFormData();
-            }}
-            color='primary'
-          >
-            Выход
-          </Button>
-          <Button
-            disabled={
-              !name__Group_Expense || name__GROUP_EXPENSE_Helper.length !== 0
-            }
-            variant='contained'
-            onClick={() => onSubmit()}
-            color='primary'
-          >
-            {buttonText ? buttonText : 'Поехали'}
-          </Button>
-        </DialogActions>
-      </Dialog>
     </Grid>
   );
 };
 
 GroupExpense.propTypes = {
-  add__GROUP_EXPENSE: PropTypes.func.isRequired,
+  setNameOfPage: PropTypes.func.isRequired,
   getAll__GROUP_EXPENSE: PropTypes.func.isRequired,
-  getOne__GROUP_EXPENSE: PropTypes.func.isRequired,
-  update__GROUP_EXPENSE: PropTypes.func.isRequired,
   delete__GROUP_EXPENSE: PropTypes.func.isRequired,
 
   // state_auth: PropTypes.object.isRequired,
@@ -320,9 +165,7 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps, {
-  add__GROUP_EXPENSE,
+  setNameOfPage,
   getAll__GROUP_EXPENSE,
-  getOne__GROUP_EXPENSE,
-  update__GROUP_EXPENSE,
   delete__GROUP_EXPENSE,
 })(GroupExpense);
--- client/src/pages/referenceData/groupExpense/GroupExpenseAdd.js
+++ client/src/pages/referenceData/groupExpense/GroupExpenseAdd.js
@@ -1,7 +1,159 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const GroupExpenseAdd = () => {
-  return <div>GroupExpenseAdd</div>;
+import { setNameOfPage } from '../../../store/actions/nameOfPage';
+import { add__GROUP_EXPENSE } from '../../../store/actions/referenceData/groupExpense';
+
+import Fab from '@material-ui/core/Fab';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    margin: 'auto',
+  },
+  item: {
+    width: '100%',
+    marginBottom: '2em',
+  },
+  dialogItem: {
+    width: '100%',
+  },
+}));
+
+const GroupExpenseAdd = ({
+  setNameOfPage,
+  add__GROUP_EXPENSE,
+
+  // state__GROUP_EXPENSE,
+}) => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const [formData, setFormData] = useState({
+    name__Group_Expense: '',
+  });
+
+  const { name__Group_Expense } = formData;
+
+  const [name__GROUP_EXPENSE_Helper, set_name__GROUP_EXPENSE_Helper] = useState(
+    ''
+  );
+
+  const clearFormData = () => {
+    setFormData({
+      name__Group_Expense: '',
+    });
+  };
+
+  useEffect(() => {
+    setNameOfPage('Добавить группу');
+
+    return () => {
+      clearFormData();
+    };
+  }, [setNameOfPage]);
+
+  const onSubmit = () => {
+    add__GROUP_EXPENSE(name__Group_Expense);
+
+    clearFormData();
+    history.goBack();
+  };
+
+  const onChangeHandler = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+
+    let valid;
+    switch (event.target.id) {
+      case 'name__Group_Expense':
+        valid = event.target.value.length >= 3;
+        if (!valid) {
+          set_name__GROUP_EXPENSE_Helper('Минимальная длина 3 знака');
+        } else {
+          set_name__GROUP_EXPENSE_Helper('');
+        }
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  return (
+    <Grid container className={classes.root} direction='column'>
+      <Tooltip title='Назад'>
+        <Fab
+          color='secondary'
+          aria-label='go Back'
+          onClick={() => history.goBack()}
+        >
+          <ArrowBackIcon />
+        </Fab>
+      </Tooltip>
+
+      <Grid item className={classes.item}>
+        <Typography variant='h3' align='center'>
+          Добавить группу расходов
+        </Typography>
+      </Grid>
+
+      <Grid item className={classes.item}>
+        <TextField
+          autoFocus
+          id='name__Group_Expense'
+          name='name__Group_Expense'
+          label='Группа расходов'
+          type='text'
+          value={name__Group_Expense ? name__Group_Expense : ''}
+          error={name__GROUP_EXPENSE_Helper.length !== 0}
+          helperText={name__GROUP_EXPENSE_Helper}
+          fullWidth
+          autoComplete='text'
+          onChange={(e) => onChangeHandler(e)}
+        />
+      </Grid>
+
+      <Grid item className={classes.item}>
+        <Button
+          disabled={
+            !name__Group_Expense || name__GROUP_EXPENSE_Helper.length !== 0
+          }
+          variant='contained'
+          onClick={() => onSubmit()}
+          color='primary'
+        >
+          Добавить
+        </Button>
+      </Grid>
+    </Grid>
+  );
 };
 
-export default GroupExpenseAdd;
+GroupExpenseAdd.propTypes = {
+  setNameOfPage: PropTypes.func.isRequired,
+  add__GROUP_EXPENSE: PropTypes.func.isRequired,
+
+  // state_auth: PropTypes.object.isRequired,
+  // state__GROUP_EXPENSE: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  // state_auth: state.auth,
+  // state__GROUP_EXPENSE: state.groupExpense,
+});
+
+export default connect(mapStateToProps, {
+  setNameOfPage,
+  add__GROUP_EXPENSE,
+})(GroupExpenseAdd);
